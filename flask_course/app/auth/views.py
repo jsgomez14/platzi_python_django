9697edc8069b99ef8d5ef52d5e3a1ad9e3a1ac26
Juanse <juancgomdotpad@gmail.com>
2021-08-23 21:30:40 -0500

    course finished

--- flask_course/app/auth/views.py
+++ flask_course/app/auth/views.py
@@ -1,7 +1,11 @@
 from flask import render_template,session,flash,redirect,url_for
+from flask_login import login_user,login_required,logout_user
+from werkzeug.security import generate_password_hash
 from app.forms import LoginForm
+from app.models import UserData,UserModel
 
 from . import auth
+from app.firestore_service import get_user,user_put
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -11,8 +15,61 @@ def login():
     }
 
     if login_form.validate_on_submit():
-        session['username'] = login_form.username.data
-        flash('Nombre de usuario registrado con éxito!')
+        username = login_form.username.data
+        password = login_form.password.data
+
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is not None:
+            password_from_db = user_doc.to_dict()['password']
+            if password == password_from_db:
+                user_data = UserData(username, password)
+                user = UserModel(user_data)
+
+                login_user(user)
+
+                flash('Bienvenido de nuevo!')
+                redirect(url_for('hello'))
+            else:
+                flash('La información no coincide!')
+        else:
+            flash('El usuario no existe!')
         return redirect(url_for('index'))
 
     return render_template('login.html', **context)
+
+@auth.route('signup',methods=['GET', 'POST'])
+def signup():
+    signup_form = LoginForm()
+
+    context = {
+        'signup_form' : signup_form
+    }
+
+    if signup_form.validate_on_submit():
+        username=signup_form.username.data
+        password=signup_form.password.data
+
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is None:
+            password_hash = generate_password_hash(password)
+            user_data = UserData(username,password_hash)
+            user_put(user_data)
+
+            user = UserModel(user_data)
+            login_user(user)
+            flash('Bienvenido!')
+            return redirect(url_for('hello'))
+
+        else:
+            flash('El usuario existe!')
+    return render_template('signup.html', **context)
+
+@auth.route('logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Regresa pronto!')
+
+    return redirect(url_for('auth.login'))
--- flask_course/main.py
+++ flask_course/main.py
@@ -1,12 +1,12 @@
 from flask import request,make_response,redirect,render_template,url_for,flash,session
 import unittest
+from flask_login import login_required, current_user
 
 from app import create_app
-from app.forms import LoginForm
+from app.forms import TodoForm, DeleteTodoForm, UpdateTodoForm
+from app.firestore_service import get_users,get_todos,put_todo,delete_todo,update_todo
 
 app = create_app()
-
-todos = ['TODO 1', 'TODO 2', 'TODO 3']
 
 @app.cli.command()
 def test():
@@ -29,13 +29,39 @@ def index():
     session['user_ip'] = user_ip
     return response
 
-@app.route('/hello', methods=['GET'])
+@app.route('/hello', methods=['GET', 'POST'])
+@login_required
 def hello():
     user_ip = session.get('user_ip')
-    username = session.get('username')
+    username = current_user.id
+    todo_form = TodoForm()
+    delete_form = DeleteTodoForm()
+    update_form = UpdateTodoForm()
+
     context = {
         'user_ip': user_ip,
-        'todos': todos,
+        'todos': get_todos(username),
         'username': username,
+        'todo_form': todo_form,
+        'delete_form': delete_form,
+        'update_form': update_form,
     }
+
+    if todo_form.validate_on_submit():
+        put_todo(username, todo_form.description.data)
+        flash('Tu tarea se creó con éxito.')
+        return redirect(url_for('hello'))
     return render_template('hello.html', **context)
+
+@app.route('/todos/delete/<todo_id>', methods=['POST'])
+def delete(todo_id):
+    user_id = current_user.id
+    delete_todo(user_id,todo_id)
+    return redirect(url_for('hello'))
+
+@app.route('/todos/update/<todo_id>/<int:done>', methods=['POST'])
+def update(todo_id,done):
+    user_id = current_user.id
+    update_todo(user_id,todo_id,done)
+    return redirect(url_for('hello'))
+    

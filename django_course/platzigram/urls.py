6b3315499fc1905ca7fe086472257b6e537024c1
Juanse <juancgomdotpad@gmail.com>
2021-09-02 12:45:30 -0500

    django course

---
+++ django_course/platzigram/urls.py
@@ -0,0 +1,12 @@
+"""Platzigram URLs"""
+
+from django.urls import path
+from . import views as local_views
+from posts import views as posts_views
+
+urlpatterns = [
+    path('hello-world',local_views.hello_world),
+    path('sorted',local_views.sort_integers), 
+    path('hi/<str:name>/<int:age>/',local_views.say_hi),
+    path('posts/',posts_views.list_posts)
+]

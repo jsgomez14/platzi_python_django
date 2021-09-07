29fb37f05ddabcf9d8af77f4c3842274eb719aa4
Juanse <juancgomdotpad@gmail.com>
2021-09-06 20:53:13 -0500

    logout, signup, middleware, more..

--- django_course/users/views.py
+++ django_course/users/views.py
@@ -2,7 +2,20 @@
 
 #Django
 from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate,login,logout
+
+# Models
+from django.contrib.auth.models import User
+from users.models import Profile
+
+# Exceptions
+from django.db.utils import IntegrityError
+
+
+def update_profile(request):
+    """Updates a user's profile view."""
+    return render(request,'users/update_profile.html')
 
 def login_view(request):
     """Login view."""
@@ -17,3 +30,35 @@ def login_view(request):
         else:
             return render(request, 'users/login.html', {'error': 'Invalid username or password'})
     return render(request, 'users/login.html')
+
+def signup_view(request):
+    """Signup view."""
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirmation = request.POST.get('password_confirmation')
+        if password != password_confirmation:
+            return render(request, 'users/signup.html', {'error':'Password confirmation does not match.'})
+        try:
+            user = User.objects.create_user(username=username, password=password)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error':'Username is already in use.'})
+
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
+    return render(request, 'users/signup.html')
+
+    
+
+@login_required
+def logout_view(request):
+    """Logout view."""
+    logout(request)
+    return redirect('login')
